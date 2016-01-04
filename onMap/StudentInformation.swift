1a40fe136360ed579228543d78e5f9b21eee9a56
Christian Zuniga <christiandzuniga@christians-macbook-air.local>
2016-01-03 18:28:25 -0800

    Used hardcoded locations to simulate downloaded data. Tested strict for student records. Put pins on the map

---
+++ onMap/StudentInformation.swift
@@ -0,0 +1,29 @@
+//
+//  StudentInformation.swift
+//  onMap
+//
+//  Created by Christian D. Zuniga on 1/3/16.
+//  Copyright © 2016 ZTechnology. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+public struct StudentInformation{
+    
+    var firstName: String
+    var lastName: String
+    var latitude: CLLocationDegrees
+    var longitude: CLLocationDegrees
+    var mediaURL: String
+    
+ 
+    /*
+    public init(first: String, last: String , lat: CLLocationDegrees, long: CLLocationDegrees){
+        firstName = first
+        lastName = last
+        latitude = lat
+        longitude = long
+    }
+   */
+}
