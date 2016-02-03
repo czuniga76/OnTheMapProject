8cc49ca237c6d9fbd89c72ff59285f91e084465b
Christian Zuniga <christiandzuniga@christians-macbook-air.local>
2016-02-03 01:14:48 -0800

    Added name field and password field. Added task for Post method to client to login through audacity. If response is received and json is parsed, the map view controller is launched. Need to check actual session id and provide more error handling.

--- onMap/LoginViewController.swift
+++ onMap/LoginViewController.swift
@@ -11,9 +11,42 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
     
     @IBAction func loginUD(sender: UIButton) {
-        let controller = self.storyboard!.instantiateViewControllerWithIdentifier("ParseController") as! UIViewController
-        self.presentViewController(controller, animated: true, completion: nil)
+        
+        let userDic: [String:AnyObject] = [
+            "username": nameField.text!,
+            "password": passwordField.text!
+        ]
+        let body : [String:AnyObject] = [
+            "udacity": userDic
+        ]
+        
+        let mutableMethod = "session"
+        
+        
+        
+        var parameters = [String: AnyObject] ()
+        let task = onMapClient.sharedInstance().taskForPOSTMethod(mutableMethod, parameters: parameters, jsonBody: body) { JSONResult, error in
+            
+            if let error = error {
+                //self.completionHandler(nil, error)
+                print("error")
+            } else {
+                print("success")
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    let controller = self.storyboard!.instantiateViewControllerWithIdentifier("ParseController") as! UIViewController
+                    self.presentViewController(controller, animated: true, completion: nil)
+                }
+                
+            }
+        }
+
+        
     }
 }
--- onMap/OnMapClient.swift
+++ onMap/OnMapClient.swift
@@ -81,6 +81,143 @@ class onMapClient : NSObject {
     }
     
     
+    
+    func taskForPOSTMethod(method:String, parameters: [String : AnyObject], jsonBody: [String:AnyObject], completionHandler: (result: AnyObject!, error: NSError?) -> Void) -> NSURLSessionDataTask {
+        
+        var mutableParameters = parameters
+       // "https://www.udacity.com/api/"
+       // let urlString = Constants.BaseURLSecure + method + onTheMapClient.escapedParameters(mutableParameters)
+        let urlString = "https://www.udacity.com/api/" + method + onMapClient.escapedParameters(mutableParameters)
+        
+        let url = NSURL(string: urlString)!
+        let request = NSMutableURLRequest(URL: url)
+        
+        // use to test directly
+        //let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+        
+        
+        var jsonifyError: NSError? = nil
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        
+        
+        
+        do {
+            
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(jsonBody, options: [])
+            
+            /*
+            request.HTTPBody = "{\"udacity\": {\"username\": \"christian.zuniga@hotmail.com\", \"password\": \"\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+            */
+            
+            /* use to check requestbody
+            let requestString = String(data: request.HTTPBody!, encoding:
+            NSUTF8StringEncoding)
+            print(requestString)
+            */
+            
+        } catch let error as NSError {
+            jsonifyError = error
+            request.HTTPBody = nil
+        }
+        
+        /* 4. Make the request */
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) {data, response, downloadError in
+            
+            /* 5/6. Parse the data and use the data (happens in completion handler) */
+            let newData = data!.subdataWithRange(NSMakeRange(5, data!.length - 5))
+            if let error = downloadError {
+                let newError = onMapClient.errorForData(newData, response: response, error: error)
+                print ("download error")
+                completionHandler(result: nil, error: downloadError)
+            } else {
+                
+                onMapClient.parseJSONWithCompletionHandler(newData, completionHandler: completionHandler)
+            }
+        }
+        
+        /* 7. Start the request */
+        task.resume()
+        
+        return task
+    }
+
+    class func errorForData(data: NSData?, response: NSURLResponse?, error: NSError) -> NSError {
+        
+        if let parsedResult = (try? NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments)) as? [String : AnyObject] {
+            
+            if let errorMessage = parsedResult[onMapClient.JSONResponseKeys.StatusMessage] as? String {
+                
+                let userInfo = [NSLocalizedDescriptionKey : errorMessage]
+                
+                return NSError(domain: "OntheMap Error", code: 1, userInfo: userInfo)
+            }
+        }
+        
+        return error
+    }
+    
+    class func parseJSONWithCompletionHandler(data: NSData, completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
+        
+        var parsingError: NSError? = nil
+        
+        // print(data)
+        
+        //let parsedResult: AnyObject?
+        //let parsedResult: NSDictionary?
+        var parsedResult = [String:AnyObject]()
+        do {
+            parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments) as! Dictionary<String,AnyObject>
+            
+            
+            //print(parsedResult)
+            /*
+            for (pkeys,pvalues) in parsedResult {
+            print(pkeys,pvalues)
+            }
+            */
+            print(parsedResult["session"]!)
+            
+        } catch let error as NSError {
+            parsingError = error
+            // parsedResult = nil
+        }
+        
+        if let error = parsingError {
+            print("parsing error in json serialization")
+            completionHandler(result: nil, error: error)
+        } else {
+            completionHandler(result: parsedResult, error: nil)
+        }
+    }
+
+    
+    
+    class func escapedParameters(parameters: [String : AnyObject]) -> String {
+        
+        var urlVars = [String]()
+        
+        for (key, value) in parameters {
+            
+            /* Make sure that it is a string value */
+            let stringValue = "\(value)"
+            
+            /* Escape it */
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            
+            /* Append it */
+            urlVars += [key + "=" + "\(escapedValue!)"]
+            
+        }
+        
+        return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
+    }
+    
+
+    
     class func sharedInstance() -> onMapClient {
         struct Singleton {
             static var sharedInstance = onMapClient()
